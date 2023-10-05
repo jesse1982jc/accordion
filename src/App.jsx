7dcea565afd655d6a664939db818a8e6060536b3
Jesse <ya710728@gmail.com>
2023-10-06 00:15:35 +0800

    commit version 2

--- src/App.jsx
+++ src/App.jsx
@@ -25,27 +25,58 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [currOpen, setCurrOpen] = useState(null);
+
   return (
     <div className="accoridion">
       {data.map((item, i) => (
         <AccordionItem
+          currOpen={currOpen}
+          onOpen={setCurrOpen}
           key={item.title}
           num={i}
           title={item.title}
-          text={item.text}
-        />
+        >
+          {item.text}
+        </AccordionItem>
       ))}
+
+      <AccordionItem
+        currOpen={currOpen}
+        onOpen={setCurrOpen}
+        key="test 1"
+        num={22}
+        title="Test 1"
+      >
+        <p>Allows React developers to:</p>
+        <ul>
+          <li>Break up UI into components</li>
+          <li>Make components reusuable</li>
+          <li>Place state efficiently</li>
+        </ul>
+      </AccordionItem>
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
+function AccordionItem({ currOpen, onOpen, num, title, children }) {
+  // isOpen 不是 true 就是 false
+  // 先判斷 num 是否嚴格等於 currOpen，再丟給 boolean 值 給 isOpen
+  const isOpen = num === currOpen;
+
+  console.log(`isOpen: ${isOpen}`);
+  console.log(`num: ${num}`);
+  console.log(`currOpen: ${currOpen}`);
+
   // 設定一個 item 細項開關的狀態
   // 預設是 false
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
 
   const handleToggle = () => {
-    setIsOpen((prevIsOpen) => !prevIsOpen);
+    // setIsOpen((prevIsOpen) => !prevIsOpen);
+
+    // isOpen 用來判斷細項是否有打開
+    onOpen(isOpen ? null : num);
   };
 
   return (
@@ -54,7 +85,7 @@ function AccordionItem({ num, title, text }) {
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
 
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
